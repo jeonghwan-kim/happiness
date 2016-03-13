b7e4f6753e3f87f75befa27df277392b16d080b0
jeonghwan <ej88ej@gmail.com>
2016-03-13 09:19:22 +0800

    Init

---
+++ HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Chris on 2016. 3. 13..
+//  Copyright © 2016년 WePlanet. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+}
